08c836d5694fb6030be9cbbee3cca1aa450ca1ea
lpch20 <lpch20@hotmail.com>
2023-11-27 15:57:30 -0300

    changes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,35 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './navbar.css'
 
 function NavBar() {
-
   return (
-    <div>
-        <div>
-          <img src="" alt="" />
-        </div>
-
-        <div>
-            <img src="" alt="" />
-        </div>
-    </div>
-  )
+    <Navbar expand="lg" className="dark">
+      <Container>
+        <Navbar.Brand id='colorHome' href="#home">&gt;devluciano&lt;</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown> */}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default NavBar
+export default NavBar;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
 import 'animate.css'
 import './home.css'
+import NavBar from "./../../components/NavBar/NavBar.jsx"
+import "animate.css";
 
 function Home() {
   return (
-    <div className='containerHome animate__animated animate__flipInY'></div>
+    <div className='containerHome animate__animated animate__flipInY'>
+
+      <header>
+        <NavBar></NavBar>
+      </header>
+
+      <main className='containerMain'>
+        <section className='firstSection'>
+
+          <div className='titleHome'>
+            <h1>Hi!</h1>
+            <h1 className='name'>Welcome</h1>
+            {/* <img className='profileImg readMore animate__animated animate__zoomInRight' src="/profile.jpg" alt="" /> */}
+          </div>
+{/* 
+
+          <div className='containerImg'>
+          
+          </div> */}
+
+        </section>
+
+        <section className='secondSection'>
+          <div className='whatIDoSection'>
+            <h1>
+              What I do?
+            </h1>
+          </div>
+        </section>
+      </main>
+
+      <footer>
+
+      </footer>
+
+    </div>
   )
 }
 
