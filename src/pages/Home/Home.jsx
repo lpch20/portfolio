1ec40258712d6d3268750d31bd37a0da08c08c4f
lpch20 <lpch20@hotmail.com>
2023-11-27 21:10:00 -0300

    add section two

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,47 +1,51 @@
-import 'animate.css'
-import './home.css'
-import NavBar from "./../../components/NavBar/NavBar.jsx"
+import "animate.css";
+import "./home.css";
+import NavBar from "./../../components/NavBar/NavBar.jsx";
 import "animate.css";
 
 function Home() {
   return (
-    <div className='containerHome animate__animated animate__flipInY'>
-
+    <div className="containerHome animate__animated animate__flipInY">
       <header>
         <NavBar></NavBar>
       </header>
 
-      <main className='containerMain'>
-        <section className='firstSection'>
-
-          <div className='titleHome'>
+      <main className="containerMain">
+        <section className="firstSection">
+          <div className="titleHome">
             <h1>Hi!</h1>
-            <h1 className='name'>Welcome</h1>
+            <h1 className="name">Welcome</h1>
             {/* <img className='profileImg readMore animate__animated animate__zoomInRight' src="/profile.jpg" alt="" /> */}
           </div>
-{/* 
 
-          <div className='containerImg'>
-          
-          </div> */}
-
-        </section>
-
-        <section className='secondSection'>
-          <div className='whatIDoSection'>
-            <h1>
-              What I do?
-            </h1>
+          <div className="loader">
+            <div data-glitch="Swipe for more...." className="glitch">
+              Swipe for more....
+            </div>
           </div>
         </section>
+
+        <section className="secondSection">
+          <div className="whatIDoSection">
+            <h1>What I do?</h1>
+          </div>
+          <div>
+            <button className="learn-more">
+              <span className="circle" aria-hidden="true">
+                <span className="icon arrow"></span>
+              </span>
+              <span className="button-text">Learn More</span>
+            </button>
+            
+          </div>
+        
+        </section>
+        <div className="card">hola</div>
       </main>
 
-      <footer>
-
-      </footer>
-
+      <footer></footer>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
