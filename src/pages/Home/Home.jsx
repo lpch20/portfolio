47eebcb3a0496f26598a63324557f872b418a818
lpch20 <lpch20@hotmail.com>
2023-11-30 10:42:37 -0300

    add cards at sectionFour

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,10 +4,12 @@ import NavBar from "./../../components/NavBar/NavBar.jsx";
 import "animate.css";
 import { useState } from "react";
 
+
 function Home() {
   const [modal, setModal] = useState(false);
   const [modal2, setModal2] = useState(false);
   const [modal3, setModal3] = useState(false);
+
 
   const handleStateModal = () => {
     if (modal) {
@@ -17,6 +19,7 @@ function Home() {
     }
   };
 
+
   const handleStateModal2 = () => {
     if (modal2) {
       setModal2(false);
@@ -24,6 +27,7 @@ function Home() {
       setModal2(true);
     }
   };
+
 
   const handleStateModal3 = () => {
     if (modal3) {
@@ -33,11 +37,13 @@ function Home() {
     }
   };
 
+
   return (
     <div className="containerHome animate__animated animate__flipInY">
       <header>
         <NavBar></NavBar>
       </header>
+
 
       <main className="containerMain">
         <section className="firstSection">
@@ -47,12 +53,14 @@ function Home() {
             {/* <img className='profileImg readMore animate__animated animate__zoomInRight' src="/profile.jpg" alt="" /> */}
           </div>
 
+
           <div className="loader">
             <div data-glitch="Swipe for more...." className="glitch">
               Swipe for more....
             </div>
           </div>
         </section>
+
 
         <section className="secondSection">
           {!modal ? (
@@ -137,6 +145,7 @@ function Home() {
           )}
         </section>
 
+
         <section className="thirdSection">
           {!modal2 ? (
             <div>
@@ -180,6 +189,7 @@ function Home() {
           )}
         </section>
 
+
         <section className="fourSection">
           {!modal3 ? (
             <div className="titleWork">
@@ -198,6 +208,46 @@ function Home() {
               </div>
               <div className="containerProjects">
                 <div className="cardWork">
+                  <img src="/viemventuraLinkPage.png" alt="" />
+                  <div className="card__content">
+                    <p className="card__title">Card Title</p>
+                    <p className="card__description">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco.
+                    </p>
+                  </div>
+                </div>
+                <div className="cardWork">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <path d="M20 5H4V19L13.2923 9.70649C13.6828 9.31595 14.3159 9.31591 14.7065 9.70641L20 15.0104V5ZM2 3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918C2.44405 21 2 20.5551 2 20.0066V3.9934ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z"></path>
+                  </svg>
+                  <div className="card__content">
+                    <p className="card__title">Card Title</p>
+                    <p className="card__description">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco.
+                    </p>
+                  </div>
+                </div>
+                <div className="cardWork">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <path d="M20 5H4V19L13.2923 9.70649C13.6828 9.31595 14.3159 9.31591 14.7065 9.70641L20 15.0104V5ZM2 3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918C2.44405 21 2 20.5551 2 20.0066V3.9934ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z"></path>
+                  </svg>
+                  <div className="card__content">
+                    <p className="card__title">Card Title</p>
+                    <p className="card__description">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco.
+                    </p>
+                  </div>
+                </div>
+                <div className="cardWork">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path d="M20 5H4V19L13.2923 9.70649C13.6828 9.31595 14.3159 9.31591 14.7065 9.70641L20 15.0104V5ZM2 3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918C2.44405 21 2 20.5551 2 20.0066V3.9934ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z"></path>
                   </svg>
@@ -215,25 +265,16 @@ function Home() {
             </div>
           )}
         </section>
-
-        <section className="fourSection">
-          <div className="titleMyWork">
-            <h1>My work</h1>
-            <button className="learn-more">
-              <span className="circle" aria-hidden="true">
-                <span className="icon arrow"></span>
-              </span>
-              <span onClick={handleStateModal} className="button-text">
-                Learn More
-              </span>
-            </button>
-          </div>
-        </section>
       </main>
+
 
       <footer></footer>
     </div>
   );
 }
 
+
 export default Home;
+
+
+
